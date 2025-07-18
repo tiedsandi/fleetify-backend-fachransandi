98387f6f4afb759472f7e151edb88b0f92359283
tiedsandi <fachransandi@gmail.com>
2025-07-19 06:59:46 +0700

    feat: Implement CORS middleware and register routes for the server

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/tiedsandi/fleetify-backend-fachransandi/routes"
+)
 
 func main() {
-	fmt.Println("Halo dunia, ini backend-nya Sandi!")
+	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		// AllowOrigins:     []string{"http://localhost:5173"},
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	routes.RegisterRoutes(server)
+	server.Run()
 }
---
+++ routes/routes.go
@@ -0,0 +1,10 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(router *gin.Engine) {
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+}
